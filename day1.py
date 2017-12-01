7935982d9cba2bbbb3c6632d8264b9463318e278
Galen Corey <galen.corey@gmail.com>
2017-12-01 17:32:39 -0500

    generalized solution to work with both challenges

--- day1.py
+++ day1.py
@@ -1,12 +1,14 @@
 input_file = open('day1input.txt', 'r')
 input = input_file.read()
 
-def addAllIfDuplicate(list):
+def addAllIfDuplicate(list, frac = None):
   #take a list of ints and sum the ones that are equal to the next one
   sum = 0
   length = len(list)
+  step = length / frac if frac else 1
+
   for i in range(length):
-    if list[i] == list[(i+1)%length]:
+    if list[i] == list[(i+step)%length]:
       sum += list[i]
   return sum
 
@@ -22,4 +24,4 @@ def splitStringByDigit(str):
 # print(addAllIfDuplicate(splitStringByDigit('1234')))
 # print(addAllIfDuplicate(splitStringByDigit('91212129')))
 
-print(addAllIfDuplicate(splitStringByDigit(input)))
+print(addAllIfDuplicate(splitStringByDigit(input), 2))
