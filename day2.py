a00a15b1db2c8cdfb6b0532b8d4b7be80dc33dd6
Galen Corey <galen.corey@gmail.com>
2017-12-02 21:46:05 -0500

    solution for day 2 part 2

--- day2.py
+++ day2.py
@@ -1,12 +1,13 @@
 input_file = open('day2input.txt', 'r')
-input = list(map(lambda l: l.split(), input_file.read().split('\n')))
+def split_and_convert_to_ints(str):
+  return list(map(lambda s: int(s), str.split()))
+input = list(map(split_and_convert_to_ints, input_file.read().split('\n')))
 
 def diff_between_largest_and_smallest(list):
   if len(list):
     largest = None
     smallest = None
     for num in list:
-      num = int(num)
       if smallest is None or num < smallest:
         smallest = num
       if largest is None or num > largest:
@@ -14,13 +15,27 @@ def diff_between_largest_and_smallest(list):
     return largest - smallest
   return 0
 
-def get_checksum(list):
+def quotient_of_divisible(list):
+  if len(list):
+    sorted_list = sorted(list)
+    for i in range(len(sorted_list)):
+      for j in range(i + 1, len(sorted_list)):
+        if (sorted_list[j]%sorted_list[i] == 0):
+          return sorted_list[j] / sorted_list[i];
+  return 0
+
+
+def get_checksum(list, fn):
   sum = 0
   for sublist in list:
-    sum += diff_between_largest_and_smallest(sublist)
+    sum += fn(sublist)
   return sum
 
-print(get_checksum(input))
+
+
+print(get_checksum(input, diff_between_largest_and_smallest))
+print(get_checksum(input, quotient_of_divisible))
+
 
 # print(diff_between_largest_and_smallest([5, 1, 9, 5]))
 # print(diff_between_largest_and_smallest([7, 5, 3]))
